111082e98fae50a09fd431660b111c933e89065d
7wondersgroupinc <Richard@7wondersgroupinc.com>
2025-07-03 01:36:03 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ function checkCode() {
   if (code === '1964') {
     window.location.href = "mailto:jessica@7wondersgroupinc.com";
   } else {
-    document.getElementById('message').innerText = "Incorrect code. Access denied.";
+    document.getElementById('message').innerText = "Incorrect code.";
+  }
+}
+function checkCEO() {
+  const code = document.getElementById('ceoCode').value;
+  if (code === 'richardonly') {
+    window.location.href = "janet.html";
+  } else {
+    document.getElementById('ceoMessage').innerText = "Access denied.";
   }
 }
